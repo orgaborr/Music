f2290977a3c923e8d0d988dd22ee80f257dac8d4
Gabor <orbangabeszb@gmail.com>
2019-11-28 09:49:15 +0100

    initial commit

---
+++ src/com/orgabor/Main.java
@@ -0,0 +1,22 @@
+package com.orgabor;
+
+import com.orgabor.model.DataSource;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DataSource dataSource = new DataSource();
+		if(!dataSource.open()) {
+			System.out.println("Can't open datasource");
+			return;
+		}
+		
+		
+		
+		
+		
+		dataSource.close();
+
+	}
+
+}
