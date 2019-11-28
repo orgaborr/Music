0616c9fa9136ea76d4d1ae58d44d5f8b38b88c8b
Gabor <orbangabeszb@gmail.com>
2019-11-28 11:48:12 +0100

    column index constants added to DataSource

--- src/com/orgabor/model/DataSource.java
+++ src/com/orgabor/model/DataSource.java
@@ -17,16 +17,25 @@ public class DataSource {
 	public static final String COLUMN_ALBUM_ID = "_id";
 	public static final String COLUMN_ALBUM_NAME = "name";
 	public static final String COLUMN_ALBUM_ARTIST = "artist";
+	public static final int INDEX_ALBUM_ID = 1;
+	public static final int INDEX_ALBUM_NAME = 2;
+	public static final int INDEX_ALBUM_ARTIST = 3;
 	
 	public static final String TABLE_ARTISTS = "artists";
 	public static final String COLUMN_ARTIST_ID = "_id";
 	public static final String COLUMN_ARTIST_NAME = "name";
+	public static final int INDEX_ARTIST_ID = 1;
+	public static final int INDEX_ARTIST_NAME = 2;
 	
 	public static final String TABLE_SONGS = "songs";
 	public static final String COLUMN_SONG_ID = "_id";
 	public static final String COLUMN_SONG_TRACK = "track";
 	public static final String COLUMN_SONG_TITLE = "title";
 	public static final String COLUMN_SONG_ALBUM = "album";
+	public static final int INDEX_SONG_ID = 1;
+	public static final int INDEX_SONG_TRACK = 2;
+	public static final int INDEX_SONG_TITLE = 3;
+	public static final int INDEX_SONG_ALBUM = 4;
 	
 	private Connection conn;
 	
